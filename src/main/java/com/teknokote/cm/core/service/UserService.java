a06f9347060bf64ce2266dd7a85ef59bcd8df594
Hichem MONCER <hmoncer@gmail.com>
2024-03-28 16:38:55 +0100

    Réinitialisation du modèle

--- src/main/java/com/teknokote/cm/core/service/UserService.java
+++ src/main/java/com/teknokote/cm/core/service/UserService.java
@@ -2,7 +2,6 @@ package com.teknokote.cm.core.service;
 
 import com.teknokote.cm.core.dao.UserDao;
 import com.teknokote.cm.dto.UserDto;
-import com.teknokote.core.dao.BasicDao;
 import com.teknokote.core.service.BaseService;
 
 public interface UserService extends BaseService<Long, UserDto>
