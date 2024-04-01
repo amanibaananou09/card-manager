b434f33493040dde3b4516db18671a54b434ae2e
Amjed_Mh <amjedmh111@gmail.com>
2024-04-01 13:23:50 +0100

    create cardGroup and listByActif

--- src/main/java/com/teknokote/cm/core/dao/CardGroupDao.java
+++ src/main/java/com/teknokote/cm/core/dao/CardGroupDao.java
@@ -3,8 +3,11 @@ package com.teknokote.cm.core.dao;
 import com.teknokote.cm.dto.CardGroupDto;
 import com.teknokote.core.dao.ActivatableDao;
 
+import java.util.List;
+
 
 public interface CardGroupDao extends ActivatableDao<Long, CardGroupDto>
 {
+    List<CardGroupDto> findAllByActif(boolean actif);
 }
 
--- src/main/java/com/teknokote/cm/core/service/CardGroupService.java
+++ src/main/java/com/teknokote/cm/core/service/CardGroupService.java
@@ -4,7 +4,9 @@ import com.teknokote.cm.dto.CardGroupDto;
 import com.teknokote.core.service.ActivatableEntityService;
 import com.teknokote.core.service.BaseService;
 
+import java.util.List;
+
 public interface CardGroupService extends ActivatableEntityService<Long, CardGroupDto>, BaseService<Long, CardGroupDto>
 {
-
+    List<CardGroupDto> findAllByActif(boolean actif);
 }
