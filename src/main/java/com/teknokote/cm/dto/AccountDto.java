7b59d062fbb49869461b96285fdb2b93f3b8a451
user <baananouamani090@gmail.com>
2024-04-15 15:27:19 +0100

    [CM_25] create customer

--- src/main/java/com/teknokote/cm/dto/AccountDto.java
+++ src/main/java/com/teknokote/cm/dto/AccountDto.java
@@ -1,26 +1,34 @@
 package com.teknokote.cm.dto;
 
+import com.teknokote.cm.core.model.EnumAccountStatus;
 import com.teknokote.core.dto.ESSActivatableDto;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 @Getter
 @Setter
-public class AccountDto extends ESSActivatableDto<Long>{
+public class AccountDto extends ESSActivatableDto<Long> {
+    private String bankName;
     private String code;
     private String label;
     private String description;
+    private EnumAccountStatus status;
     private Long customerId;
+    private Set<MovementDto> movements;
+
     @Builder
-    public AccountDto(Long id, Long version, boolean actif, LocalDateTime dateStatusChange, String code, String label, String description,Long customerId)
-    {
-        super(id,version,actif,dateStatusChange);
+    public AccountDto(Long id, Long version, boolean actif, LocalDateTime dateStatusChange, String code, String label, String description, Long customerId, String bankName, EnumAccountStatus status, Set<MovementDto> movements) {
+        super(id, version, actif, dateStatusChange);
+        this.bankName = bankName;
         this.code = code;
         this.label = label;
         this.description = description;
+        this.status = status;
         this.customerId = customerId;
+        this.movements = movements;
     }
 }
--- src/main/java/com/teknokote/cm/dto/CustomerDto.java
+++ src/main/java/com/teknokote/cm/dto/CustomerDto.java
@@ -6,17 +6,30 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 @Getter
 @Setter
-public class CustomerDto extends ESSActivatableDto<Long>{
-    private String name;
+public class CustomerDto extends ESSActivatableDto<Long> {
+    private String identifier;
+    private String address;
+    private String phone;
+    private String email;
+    private String companyName;
+    private Set<AccountDto> accounts;
     private Long supplierId;
+    private String supplierName;
+
     @Builder
-    public CustomerDto(Long id, Long version, boolean actif, LocalDateTime dateStatusChange, String name,Long supplierId)
-    {
-        super(id,version,actif,dateStatusChange);
-        this.name = name;
+    public CustomerDto(Long id, Long version, boolean actif, LocalDateTime dateStatusChange, String identifier, String address, String phone, String email, String companyName, Long supplierId, String supplierName, Set<AccountDto> accounts) {
+        super(id, version, actif, dateStatusChange);
+        this.identifier = identifier;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
+        this.companyName = companyName;
         this.supplierId = supplierId;
+        this.supplierName = supplierName;
+        this.accounts = accounts;
     }
 }
