1e93b669234266b2955b532bfa021461e86758e0
user <baananouamani090@gmail.com>
2024-05-14 09:56:26 +0100

    [CM-75] Édition de cartes

--- src/main/java/com/teknokote/cm/controller/EndPoints.java
+++ src/main/java/com/teknokote/cm/controller/EndPoints.java
@@ -35,8 +35,6 @@ public class EndPoints {
     public static final String CUSTOMER_CARD_GROUP_ROOT = CUSTOMER_BASE_ROOT + CARD_GROUP_ROOT;
     public static final String CUSTOMER_CARD_ROOT = CUSTOMER_BASE_ROOT + CARD_ROOT;
     public static final String CUSTOMER_ACCOUNT_ROOT = CUSTOMER_BASE_ROOT + ACCOUNT_ROOT;
-    public static final String TRANSACTION_ROOT = CUSTOMER_BASE_ROOT + "/transaction";
-
 
 }
 
