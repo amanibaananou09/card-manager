6a7157820e3422c350f22d02400e8ec54e8af539
mansourmanel <mansourmanel7@gmail.com>
2024-05-21 14:15:28 +0100

    [CM-73]Règles de gestion du paramétrage des groupes de cartes , modification de client

--- src/main/java/com/teknokote/cm/core/repository/CardGroupRepository.java
+++ src/main/java/com/teknokote/cm/core/repository/CardGroupRepository.java
@@ -3,6 +3,7 @@ package com.teknokote.cm.core.repository;
 import com.teknokote.cm.core.model.CardGroup;
 import com.teknokote.core.dao.JpaActivatableRepository;
 import org.springframework.beans.PropertyValues;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,6 +12,6 @@ import java.util.List;
 public interface CardGroupRepository extends JpaActivatableRepository<CardGroup, Long>
 {
     List<CardGroup> findAllByActif(Boolean actif);
-
+    @Query("select c from CardGroup c where c.customerId = :customerId order by c.audit.createdDate desc")
     List<CardGroup> findAllByCustomerId(Long customerId);
 }
